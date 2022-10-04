46b888087b4971694eedfb2e0b6fad40836b3eb2
lihua <3050539989@qq.com>
2022-10-04 20:39:28 +0800

    new

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -11,13 +11,18 @@ public class TestArrayDequeGold {
         String str = "\n";
         for(int i=1;i<=500;i++)
         {
+            if(i%5==0)
+            {
+                str += "size()\n";
+                assertEquals(str,stu.size(),sol.size());
+            }
             double op = StdRandom.uniform();
             if(op<=0.25)
             {
                 stu.addFirst(i);
                 sol.addFirst(i);
                 num++;
-                str += "addFirst(" + i + ");\n";
+                str += "addFirst(" + i + ")\n";
                 assertEquals(str,stu.get(0),sol.get(0));
             }
 
@@ -26,7 +31,7 @@ public class TestArrayDequeGold {
                 stu.addLast(i);
                 sol.addLast(i);
                 num++;
-                str += "addLast(" + i + ");\n";
+                str += "addLast(" + i + ")\n";
                 assertEquals(str,stu.get(num-1),sol.get(num-1));
             }
 
@@ -34,9 +39,14 @@ public class TestArrayDequeGold {
             {
                 if(num>0)
                 {
-                    str += "removeFirst();\n";
+                    str += "removeFirst()\n";
                     num--;
                     assertEquals(str,stu.removeFirst(),sol.removeFirst());
+                }
+                else
+                {
+                    str +="isEmpty()\n";
+                    assertEquals(str,sol.isEmpty(),stu.isEmpty());
                 }
             }
 
@@ -44,9 +54,14 @@ public class TestArrayDequeGold {
             {
                 if(num>0)
                 {
-                    str += "removeLast();\n";
+                    str += "removeLast()\n";
                     num--;
                     assertEquals(str,stu.removeLast(),sol.removeLast());
+                }
+                else
+                {
+                    str +="isEmpty()\n";
+                    assertEquals(str,sol.isEmpty(),stu.isEmpty());
                 }
             }
         }
