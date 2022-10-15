aaa3040c6e12e3cf7e6f1929623aaa6f947683fe
lihua <3050539989@qq.com>
2022-10-15 09:48:57 +0800

    new1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,42 +1,51 @@
-// TODO: Make sure to make this class a part of the synthesizer package
 package synthesizer;
-import javax.sql.rowset.FilteredRowSet;
+
 import java.util.Iterator;
-//TODO: Make sure to make this class and all of its methods public
-//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
-    private int first;            // index for the next dequeue or peek
+    private int first;
     /* Index for the next enqueue. */
     private int last;
     /* Array for storing the buffer data. */
     private T[] rb;
 
-    private int chage(int x)
-    {
-        return x%capacity;
-    }
-
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
     public ArrayRingBuffer(int capacity) {
-        rb =(T[]) new Object[capacity];
+        rb = (T[]) new Object[capacity];
         this.capacity = capacity;
         first = last = fillCount = 0;
     }
 
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then throw new
+     * RuntimeException("Ring buffer overflow"). Exceptions covered Monday.
+     */
     public void enqueue(T x) {
-        if(isFull()) throw new RuntimeException("Ring buffer overflow");
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
         rb[last++] = x;
-        last = chage(last);
+        if (last == capacity) {
+            last = 0;
+        }
         fillCount++;
     }
 
-
-
-
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then throw
+     * new RuntimeException("Ring buffer underflow"). Exceptions covered Monday.
+     */
     public T dequeue() {
-        if(isEmpty())throw new RuntimeException("Ring buffer overflow");
-        T res = rb[first];
-        first = chage(++first);
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T res = rb[first++];
+        if (first == capacity) {
+            first = 0;
+        }
         fillCount--;
         return res;
     }
@@ -45,11 +54,14 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
-
-        if(isEmpty())throw new RuntimeException("Ring buffer overflow");
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
         return rb[first];
     }
 
+    /** returns an iterator */
+    @Override
     public Iterator<T> iterator() {
         return new BufferIterator();
     }
@@ -78,5 +90,6 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
             num++;
             return returnItem;
         }
+
     }
 }
